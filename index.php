93951d36f1d61757d56378bd8a311b84cdb90d4a
Karen <bezimeninofamily@gmail.com>
2019-07-22 12:12:24 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+  <title>title</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="src/css/normalize.css">
+  <link rel="stylesheet" type="text/css" href="src/css/styles.css">
+  <script type="text/javascript" src="src/js/jquery-min.js"></script>
+  <script type="text/javascript" src="src/js/script.js"></script>
+</head>
+
+<body>
+
+  <section id="main">
+
+  </section>
+
+</body>
+
+</html>
